0619ab60263d6758c5b5e3fe47996e7099e4b833
Nguyen Viet Khanh <lemontre3x1@gmail.com>
2026-05-09 09:12:38 +0700

    feat: prepare environment for lesson04 in class

---
+++ class/lesson03-JPA/javaweb35/javaweb35/src/main/java/vn/devpro/javaweb35/Javaweb35Application.java
@@ -0,0 +1,13 @@
+package vn.devpro.javaweb35;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaweb35Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javaweb35Application.class, args);
+	}
+
+}
---
+++ class/lesson03-JPA/javaweb35/javaweb35/src/test/java/vn/devpro/javaweb35/Javaweb35ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.devpro.javaweb35;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javaweb35ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
