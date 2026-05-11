9933d57b46a1230059d86e7f228eaab0f60588ce
Nguyen Viet Khanh <lemontre3x1@gmail.com>
2026-05-11 09:27:26 +0700

    feat: lesson04-Home admin

--- class/lesson03-JPA/javaweb35/javaweb35/src/main/java/vn/devpro/javaweb35/controller/administrator/HomeAdminController.java
+++ class/lesson03-JPA/javaweb35/javaweb35/src/main/java/vn/devpro/javaweb35/controller/administrator/HomeAdminController.java
@@ -1,15 +1,16 @@
 package vn.devpro.javaweb35.controller.administrator;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 public class HomeAdminController {
-//
-//	@GetMapping({ "/home", "/", "" })
-//	public String home() {
-//		return "administrator/index";
-//	}
+
+	@GetMapping("/home")
+	public String home() {
+		return "administrator/index";
+	}
 
 }
